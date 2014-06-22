73efbc5aebc208367cf16a687fbfe6fb5e7c3572
Tymoteusz Czech <mail@tymek.cz>
2014-06-23 00:38:42 +0200

    Sprawdzanie spójności

--- assets/properties.js
+++ assets/properties.js
@@ -5,10 +5,45 @@ var properities = {
 		}
 		return "tak";
 	},
+	isConnected: function(){
+		this.visited = new Array(graph.V.length);
+
+		for(var i = 0; i < this.visited.length; ++i)
+			this.visited[i] = 0;
+
+		this.visit(0);
+
+		for(var i = 0; i < this.visited.length; ++i)
+			if (this.visited[i] == 0) return "nie";
+		return "tak";
+	},
+	visit: function(){
+		var id = arguments[0];
+		this.visited[id] = 1;
+
+		for(var k = 0; k < graph.E.length; ++k){
+			if(graph.E[k].source.id == id && this.visited[graph.E[k].target.id] == 0)
+				this.visit(graph.E[k].target.id);
+			else if(graph.E[k].target.id == id && this.visited[graph.E[k].source.id] == 0)
+				this.visit(graph.E[k].source.id);
+		}
+	},
 	rebuild: function(){
 		var t = this;
-		$("#properities span").text(function(){
-			return t[$(this).attr("data-properity")]();
+		$("#properities span").each(function(){
+			//cl(typeof t[$(this).attr("data-properity")]);
+			if(typeof t[$(this).attr("data-properity")] !== "function"){
+				$(this).parent().hide();
+				return;
+			}
+			var val = t[$(this).attr("data-properity")]();
+			if(typeof val === "undefined" || val == "nie dotyczy"){
+				$(this).parent().hide();
+			} else {
+				$(this).parent().show();
+				$(this).text(val);
+			}
+			//return t[$(this).attr("data-properity")]();
 		});
 	}
 }
